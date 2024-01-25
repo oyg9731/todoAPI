e1eb38a56ef1efb5c801a54bd5a09443d7eb63c8
oh young seok <oyg9731@naver.com>
2024-01-25 10:42:10 +0900

    #11. 할 일 처리 회원연관관계 인증로직 완성

--- src/main/java/com/study/todoapi/todo/controller/TodoController.java
+++ src/main/java/com/study/todoapi/todo/controller/TodoController.java
@@ -1,13 +1,16 @@
 package com.study.todoapi.todo.controller;
 
+import com.study.todoapi.auth.TokenUserInfo;
 import com.study.todoapi.todo.dto.request.TodoCheckRequestDTO;
 import com.study.todoapi.todo.dto.request.TodoCreateRequestDTO;
+import com.study.todoapi.todo.dto.response.TodoDetailResponseDTO;
 import com.study.todoapi.todo.dto.response.TodoListResponseDTO;
 import com.study.todoapi.todo.entity.Todo;
 import com.study.todoapi.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -21,71 +24,79 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/todos")
-//@CrossOrigin(origins = {"http://localhost:3000"})// 허용할 클라이언트 아이피
+//@CrossOrigin(origins = {"http://localhost:3000"}) // API접근을 허용할 클라이언트 아이피
 public class TodoController {
+
     private final TodoService todoService;
 
     // 할 일 등록 요청
     @PostMapping
     public ResponseEntity<?> createTodo(
-            @Validated @RequestBody
-            TodoCreateRequestDTO dto,
-            BindingResult result
-    ){
+            @AuthenticationPrincipal TokenUserInfo userInfo // 토큰에 들어있는 파싱된 로그인유저정보를 시큐리티가 주입해줌
+            , @Validated @RequestBody TodoCreateRequestDTO dto
+            , BindingResult result
+    ) {
 
-        if (result.hasErrors()){
+        if (result.hasErrors()) {
             log.warn("DTO검증 에러!! : {}", result.getFieldError());
             return ResponseEntity.badRequest().body(result.getFieldError());
         }
 
         try {
-            TodoListResponseDTO todoListResponseDTO = todoService.create(dto);
-            return ResponseEntity.ok().body(todoListResponseDTO);
-        }catch (Exception e){
+            TodoListResponseDTO dtoList = todoService.create(dto, userInfo.getEmail());
+            return ResponseEntity.ok().body(dtoList);
+        } catch (Exception e) {
             log.error(e.getMessage());
             return ResponseEntity
                     .internalServerError()
                     .body(TodoListResponseDTO.builder().error(e.getMessage()).build());
         }
+
     }
 
     // 할 일 목록 조회 요청
     @GetMapping
-    public ResponseEntity<?> retrieveTodoList(){
-        log.info("/api/toods GET!");
+    public ResponseEntity<?> retrieveTodoList(
+            @AuthenticationPrincipal TokenUserInfo userInfo
+    ) {
+        log.info("/api/todos GET! ");
 
-        TodoListResponseDTO retrieve = todoService.retrieve();
+        TodoListResponseDTO retrieve = todoService.retrieve(userInfo.getEmail());
 
         return ResponseEntity.ok().body(retrieve);
     }
 
     // 할 일 삭제 요청
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteTodo(@PathVariable String id){
+    public ResponseEntity<?> deleteTodo(@AuthenticationPrincipal TokenUserInfo userInfo, @PathVariable String id) {
 
         log.info("/api/todos/{} DELETE !!", id);
 
-        if (id == null || id.trim().equals("")){
-            return ResponseEntity.badRequest()
+        if (id == null || id.trim().equals("")) {
+            return ResponseEntity
+                    .badRequest()
                     .body(TodoListResponseDTO
                             .builder()
-                            .error("ID는 공백일 수 없습니다.")
+                            .error("ID는 공백일 수 없습니다!!")
                             .build());
         }
+
         try {
-            TodoListResponseDTO dtoList = todoService.delete(id);
+            TodoListResponseDTO dtoList = todoService.delete(id, userInfo.getEmail());
             return ResponseEntity.ok().body(dtoList);
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity
                     .internalServerError()
                     .body(TodoListResponseDTO.builder().error(e.getMessage()).build());
-
         }
+
     }
+
     // 할 일 완료 체크처리 요청
     @RequestMapping(method = {PUT, PATCH})
     public ResponseEntity<?> updateTodo(
-            @RequestBody TodoCheckRequestDTO dto
+            @AuthenticationPrincipal TokenUserInfo userInfo
+            , @RequestBody TodoCheckRequestDTO dto
             , HttpServletRequest request
     ) {
 
@@ -93,13 +104,12 @@ public class TodoController {
         log.debug("dto: {}", dto);
 
         try {
-            TodoListResponseDTO dtoList = todoService.check(dto);
+            TodoListResponseDTO dtoList = todoService.check(dto, userInfo.getEmail());
             return ResponseEntity.ok().body(dtoList);
         } catch (Exception e) {
             return ResponseEntity.internalServerError()
                     .body(TodoListResponseDTO.builder().error(e.getMessage()).build());
         }
     }
-
 
 }
